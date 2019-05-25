d5ed08b511573d15cee3ef9680261c2afa8b1590
Alain <alainperkaz@gmai..com>
2019-05-25 12:47:23 +0200

    Add eventBus

---
+++ src/utils/events.js
@@ -0,0 +1,9 @@
+const SUBMIT_EMPLOYER_OFFER = "submit_employer_offer";
+const SUBMIT_EMPLOYEE_EXPECTATION = "submit_employee_expectation";
+const COMPLETE_RESULTS = "complete_results"; // TODONOW: do we need this?
+
+export const events = {
+  SUBMIT_EMPLOYER_OFFER,
+  SUBMIT_EMPLOYEE_EXPECTATION,
+  COMPLETE_RESULTS
+};
